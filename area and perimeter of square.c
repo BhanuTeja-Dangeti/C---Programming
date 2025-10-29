cd8b9e784331b8f4147bfe4307a9ff99e1e072cc
BhanuTeja-Dangeti <dangetibhanuteja@gmail.com>
2025-10-29 11:25:40 +0530

    area and perimeter of square

---
+++ area and perimeter of square.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main ()
+{
+    int s,area,perimeter;
+    scanf("%d",&s);
+    area=s*s;
+    perimeter=4*s;
+    printf("%d %d",area,perimeter);
+    return 0;
+}
